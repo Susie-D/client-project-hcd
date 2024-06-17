c83ce4b1f8e0ec971710cb5c2e2ef3dca50ebc60
Ayla Kulifaj <Aylakulifaj@gmail.com>
2024-06-17 11:03:06 -0500

    Home Page and Database

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+  const user = useSelector(store => store.user);
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <NavLink className="navLink" to="/info">
+        Tutorial & Helpful Links
+      </NavLink>
+      <br />
+      <NavLink className="navLink" to="/info">
+        Add Devices/Items
+      </NavLink>
+      <br />
+      <NavLink className="navLink" to="/info">
+        Items Reports
+      </NavLink>
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
