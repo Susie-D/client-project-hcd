a94ec1cc5d0f97c357d907f978f2e05fad4342e2
Steven Karl <Karl.Steven09@gmail.com>
2024-06-17 16:21:44 -0500

    starting on the devices.router.js

--- server/routes/devices.router.js
+++ server/routes/devices.router.js
@@ -10,6 +10,36 @@ router.get('/', (req, res) => {
 });
 
 /**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+  // console.log('I am in the devices.router.js', req.user);
+  const query = `
+  SELECT "brand", "model", "serial_number", "maintenance_date", 
+  "maintenance_due", "location", "img_url", "manufacture_date", 
+  "install_date" FROM "devices";
+  `;
+  const userIdValues = req.user.id;
+  const values = [userIdValues]
+  pool.query(query, values)
+  .then(result => {
+      res.send(result.rows);
+  })
+  .catch(err => {
+    console.log('ERROR: Get all devices', err);
+    res.sendStatus(500)
+  })
+});
+
+
+
+
+
+
+
+
+
+/**
  * POST route template
  */
 router.post('/', (req, res) => {
