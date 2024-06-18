0e77153b87bcf084a132acf5ca1a4c9c4aee3ac8
Mahad Hassan <mahadh578@gmail.com>
2024-06-18 15:18:13 -0500

    Removed more comments on devices.router.js

--- server/routes/devices.router.js
+++ server/routes/devices.router.js
@@ -2,10 +2,6 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-
 
 router.get('/', (req, res) => {
   const query = `
