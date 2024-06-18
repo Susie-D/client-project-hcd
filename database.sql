91540dfb352c40725ae8a27ba750ebdc295eb36a
Mahad Hassan <mahadh578@gmail.com>
2024-06-18 15:56:56 -0500

    This is the current database with updated columnns and dummy data

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
--- USER 
+
 DROP TABLE IF EXISTS "additional_device_info";
 DROP TABLE IF EXISTS "device_properties";
 DROP TABLE IF EXISTS "devices";
