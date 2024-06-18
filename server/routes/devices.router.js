f929e343947f6d7fcbaca9f0fb5201aa539ca1dd
Steven Karl <Karl.Steven09@gmail.com>
2024-06-18 10:13:08 -0500

    small SQL change in devices.router.js

--- server/routes/devices.router.js
+++ server/routes/devices.router.js
@@ -7,23 +7,34 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-   console.log('I am in the devices.router.js', req.user);
+  console.log('I am in the devices.router.js', req.user);
+  
+  // Define the query with a WHERE clause to filter by user_id
   const query = `
-  SELECT "brand", "model", "serial_number", "maintenance_date", 
-  "maintenance_due", "location", "img_url", "manufacture_date", 
-  "install_date" FROM "devices";
+    SELECT "brand", "model", "serial_number", "maintenance_date", 
+    "maintenance_due", "location", "img_url", "manufacture_date", 
+    "install_date" 
+    FROM "devices"
+    WHERE "user_id" = $1;
   `;
+  
+  // Retrieve the user ID from the request object
   const userIdValues = req.user.id;
-  const values = [userIdValues]
+  
+  // Create the values array with the user ID
+  const values = [userIdValues];
+  
+  // Execute the query with parameterized values
   pool.query(query, values)
-  .then(result => {
+    .then(result => {
       res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Get all devices', err);
-    res.sendStatus(500)
-  })
+    })
+    .catch(err => {
+      console.log('ERROR: Get all devices', err);
+      res.sendStatus(500);
+    });
 });
+
 
 
 
