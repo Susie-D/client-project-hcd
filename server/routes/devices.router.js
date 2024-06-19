4c4ff0406b5ab54f5d56de70f5024abda6a65c4b
Steven Karl <Karl.Steven09@gmail.com>
2024-06-19 15:14:38 -0500

    working on devices.router.js -- looking to get device_types.name into the table

--- server/routes/devices.router.js
+++ server/routes/devices.router.js
@@ -6,8 +6,38 @@ const router = express.Router();
  * GET route template
  */
 
-//! Next router get needs to be device ID router.get('/:id', (req, res) => {
 
+//TODO we need to change around the SQL so that we JOIN both tables "user" & "device_types". We are looking to get the device_types.name for the correspoonding user.id 
+//! START ---------------
+  router.get('/:id', (req, res) => {
+    const userId = req.params.id;
+    const query = `
+      SELECT "device_types"."username"
+      FROM "device_types"
+      JOIN "user" ON "user"."name" = "device_types"."username"
+      WHERE "user"."id" = $1;
+    `;
+    const values = [userId];
+  
+    pool.query(query, values)
+      .then((dbResult) => {
+        res.send(dbResult.rows);
+      })
+      .catch((error) => {
+        console.log("THIS IS AN ERROR", error);
+        res.sendStatus(500);
+      });
+  });
+
+//! END ---------------
+
+
+
+
+
+
+
+//* VERY HAPPY WITH THIS
 
 router.get('/', (req, res) => {
   const query = `
@@ -30,5 +60,9 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   // POST route code here
 });
+
+//* VERY HAPPY WITH THIS
+
+
 
 module.exports = router;
