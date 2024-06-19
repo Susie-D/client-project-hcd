52f92f86b076981776a5e3ad07779905c91b2c3c
Susie Douang <soutsada.d@gmail.com>
2024-06-19 13:00:19 -0500

    udpate dropdown styles

--- src/pages/InitialIntake/InitialIntake.jsx
+++ src/pages/InitialIntake/InitialIntake.jsx
@@ -15,63 +15,21 @@ export default function InitialIntake() {
   return (
     <PageLayout>
       <div className="initial-intake-container column ac-center">
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">
+        <FormControl fullWidth style={{ marginTop: '30px' }}>
+          <InputLabel
+            style={{
+              fontWeight: 'bold',
+              margin: '0 15%',
+            }}
+            id="select-label"
+          >
             Heating-Ventilating-AC
           </InputLabel>
           <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
+            labelId="select-label"
+            id="select"
             value={age}
-            label="Age"
-            onChange={(e) => handleChange(e)}
-            style={{ borderRadius: '10em', backgroundColor: '#6cb051' }}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl fullWidth style={{ marginTop: '15px' }}>
-          <InputLabel id="demo-simple-select-label">Appliance</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={(e) => handleChange(e)}
-            style={{ borderRadius: '10em', backgroundColor: '#6cb051' }}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl fullWidth style={{ marginTop: '15px' }}>
-          <InputLabel id="demo-simple-select-label">Plumbing</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={(e) => handleChange(e)}
-            style={{ borderRadius: '10em', backgroundColor: '#6cb051' }}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl fullWidth style={{ marginTop: '15px' }}>
-          <InputLabel id="demo-simple-select-label">Safety</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
+            label="hvac"
             onChange={(e) => handleChange(e)}
             style={{ borderRadius: '10em', backgroundColor: '#a9d09e' }}
           >
@@ -81,16 +39,23 @@ export default function InitialIntake() {
           </Select>
         </FormControl>
 
-        <FormControl fullWidth style={{ marginTop: '15px' }}>
-          <InputLabel id="demo-simple-select-label">Structure</InputLabel>
+        <FormControl fullWidth style={{ marginTop: '30px' }}>
+          <InputLabel
+            style={{
+              fontWeight: 'bold',
+              margin: '0 33%',
+            }}
+            id="select-label"
+          >
+            Appliance
+          </InputLabel>
           <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
+            labelId="select-label"
+            id="select"
             value={age}
-            label="Age"
-            className=""
+            label="appliance"
             onChange={(e) => handleChange(e)}
-            style={{ borderRadius: '10em', backgroundColor: '#6cb051' }}
+            style={{ borderRadius: '10em', backgroundColor: '#a9d09e' }}
           >
             <MenuItem value={10}>Ten</MenuItem>
             <MenuItem value={20}>Twenty</MenuItem>
@@ -98,15 +63,95 @@ export default function InitialIntake() {
           </Select>
         </FormControl>
 
-        <FormControl fullWidth style={{ marginTop: '15px' }}>
-          <InputLabel id="demo-simple-select-label">Landscape</InputLabel>
+        <FormControl fullWidth style={{ marginTop: '30px' }}>
+          <InputLabel
+            style={{
+              fontWeight: 'bold',
+              margin: '0 33%',
+            }}
+            id="select-label"
+          >
+            Plumbing
+          </InputLabel>
           <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
+            labelId="select-label"
+            id="select"
+            value={age}
+            label="plumbing"
+            onChange={(e) => handleChange(e)}
+            style={{ borderRadius: '10em', backgroundColor: '#a9d09e' }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth style={{ marginTop: '30px' }}>
+          <InputLabel
+            style={{
+              fontWeight: 'bold',
+              margin: '0 38%',
+            }}
+            id="select-label"
+          >
+            Safety
+          </InputLabel>
+          <Select
+            labelId="select-label"
+            id="select"
+            value={age}
+            label="safety"
+            onChange={(e) => handleChange(e)}
+            style={{ borderRadius: '10em', backgroundColor: '#a9d09e' }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth style={{ marginTop: '30px' }}>
+          <InputLabel
+            style={{
+              margin: '0 35%',
+              fontWeight: 'bold',
+            }}
+            id="select-label"
+          >
+            Structure
+          </InputLabel>
+          <Select
+            labelId="select-label"
+            id="select"
+            value={age}
+            label="structure"
+            onChange={(e) => handleChange(e)}
+            style={{ borderRadius: '10em', backgroundColor: '#a9d09e' }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth style={{ marginTop: '30px' }}>
+          <InputLabel
+            style={{
+              margin: '0 33%',
+              fontWeight: 'bold',
+            }}
+            id="select-label"
+          >
+            Landscape
+          </InputLabel>
+          <Select
+            labelId="select-label"
+            id="select"
             value={age}
             label="Age"
-            onChange={(e) => handleChange(e)}
-            style={{ borderRadius: '10em', backgroundColor: '#6cb051' }}
+            onChange={() => handleChange(e)}
+            style={{ borderRadius: '10em', backgroundColor: '#a9d09e' }}
           >
             <MenuItem value={10}>Ten</MenuItem>
             <MenuItem value={20}>Twenty</MenuItem>
