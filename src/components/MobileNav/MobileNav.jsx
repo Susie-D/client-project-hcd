e735ae9846a284641dbab9680023cfe68f49a680
Susie Douang <soutsada.d@gmail.com>
2024-06-22 19:15:55 -0500

    initial mobile nav setup

---
+++ src/components/MobileNav/MobileNav.jsx
@@ -0,0 +1,12 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import '../../styles/_styles.scss';
+
+export default function MobileNav() {
+  return (
+    <>
+      <div className="mobile-nav-container jc-end">
+        <MenuIcon fontSize="large" style={{ margin: `0.5em .75em 0 0.5em ` }} />
+      </div>
+    </>
+  );
+}
--- src/components/components.jsx
+++ src/components/components.jsx
@@ -1,10 +1,11 @@
 export { default as Footer } from './Footer/Footer';
 export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as LogOutButton } from './LogOutButton/LogOutButton';
+export { default as MobileNav } from './MobileNav/MobileNav';
 export { default as Nav } from './Nav/Nav';
-export { default as SignUpForm } from './SignUpForm/SignUpForm';
 export { default as IntakeFormBase } from './IntakeForm/IntakeFormBase';
 export { default as IntakeFormFurnace } from './IntakeForm/IntakeFormFurnace';
 export { default as IntakeFormFridge } from './IntakeForm/IntakeFormFridge';
 export { default as IntakeFormUnder_Sink } from './IntakeForm/IntakeFormUnder_Sink';
 export { default as IntakeFormGutters_n_Spouts } from './IntakeForm/IntakeFormGutters_n_Spouts';
+export { default as SignUpForm } from './SignUpForm/SignUpForm';
