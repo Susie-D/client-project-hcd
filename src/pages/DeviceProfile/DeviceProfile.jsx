6a3bdfea9d13b0d4aa0707b904e05f06649f06e5
Steven Karl <Karl.Steven09@gmail.com>
2024-06-24 13:39:50 -0500

    added mahads changes for PULL

--- src/pages/DeviceProfile/DeviceProfile.jsx
+++ src/pages/DeviceProfile/DeviceProfile.jsx
@@ -4,12 +4,35 @@ import React from 'react';
 function DeviceProfile() {
 
 
-return(
-    <div>
-        Halle Berry
-    </div>
-
-)
+    return (
+        <>
+            <div>
+                <h1>yurrr</h1>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Brand</th>
+                        <td>Random</td>
+                        <th>Model</th>
+                        <td>Random</td>
+                        <th>Serial Number</th>
+                        <td>Random</td>
+                        <th>Maintenance Date</th>
+                        <td>Random</td>
+                        <th>Maintenance Due</th>
+                        <td>Random</td>
+                        <th>Location</th>
+                        <td>Random</td>
+                        <th>Manufacture Date</th>
+                        <td>Random</td>
+                        <th>Install Date</th>
+                        <td>Random</td>
+                    </tr>
+                </thead>
+            </table>
+        </>
+    );
 }
 
 
