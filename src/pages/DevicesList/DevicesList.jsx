919ff04d68c67ee9523dfb21de5beee8dc446089
Susie Douang <soutsada.d@gmail.com>
2024-06-24 16:26:42 -0500

    use datagrid MUI for display and filter table

--- src/pages/DevicesList/DevicesList.jsx
+++ src/pages/DevicesList/DevicesList.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import DevicesListItem from '../DevicesListItem/DevicesListItem';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import './_devicesList.scss';
 
 function DevicesList() {
   const dispatch = useDispatch();
@@ -12,28 +15,17 @@ function DevicesList() {
 
   return (
     <div className="container">
-      <div>
-        <h1>Devices List 😄</h1>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Brand</th>
-            <th>Model</th>
-            <th>Serial Number</th>
-            <th>Maintenance Date</th>
-            <th>Maintenance Due</th>
-            <th>Location</th>
-            <th>Manufacture Date</th>
-            <th>Install Date</th>
-          </tr>
-        </thead>
-        {devicesList.map((device) => (
-          <tbody>
-            <DevicesListItem key={device.id} device={device} />
-          </tbody>
-        ))}
-      </table>
+      <div className="header-three">Devices List 😄</div>
+      <Box
+        sx={{
+          height: '100%',
+          width: '100%',
+          marginTop: '2em',
+          fontSize: '8px',
+        }}
+      >
+        <DevicesListItem />
+      </Box>
     </div>
   );
 }
--- src/pages/DevicesListItem/DevicesListItem.jsx
+++ src/pages/DevicesListItem/DevicesListItem.jsx
@@ -1,24 +1,82 @@
 import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { useSelector } from 'react-redux';
 
+function DevicesListItem({ device }) {
+  const devicesList = useSelector((store) => store.devicesReducer.deviceItems);
 
-//! useEffect --> get item by id
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 80,
+      editable: true,
+    },
+    {
+      field: 'brand',
+      headerName: 'Brand',
+      width: 80,
+      editable: true,
+    },
+    {
+      field: 'model',
+      headerName: 'Model',
+      width: 100,
+      editable: true,
+    },
+    {
+      field: 'serial_number',
+      headerName: 'Serial #',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'maintenance_date',
+      headerName: 'Maint Date',
+      width: 115,
+      editable: true,
+    },
+    {
+      field: 'maintenance_due',
+      headerName: 'Maint Due',
+      width: 115,
+      editable: true,
+    },
+    {
+      field: 'location',
+      headerName: 'Location',
+      width: 115,
+      editable: true,
+    },
+    {
+      field: 'manufacture_date',
+      headerName: 'Mfr Date',
+      width: 115,
+      editable: true,
+    },
+    {
+      field: 'install_date',
+      headerName: 'Install Date',
+      width: 115,
+      editable: true,
+    },
+  ];
 
-function DevicesListItem({device}) {
-    return (
-        <tr>
-            {/* {JSON.stringify(device)} */}
-            <td>{device.brand}</td>
-            <td>{device.model}</td>
-            <td>{device.serial_number}</td>
-            <td>{device.maintenance_date}</td>
-            <td>{device.maintenance_due}</td>
-            <td>{device.location}</td>
-            <td>{device.manufacture_date}</td>
-            <td>{device.install_date}</td>
-        </tr>
-    );
+  return (
+    <DataGrid
+      rows={devicesList}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 5,
+          },
+        },
+      }}
+      pageSizeOptions={[5]}
+      enableRowSelectionOnClick
+    />
+  );
 }
 
 export default DevicesListItem;
-
-
